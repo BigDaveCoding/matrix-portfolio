0958d3bd992cad1671bdbe41e74f7b14d76706bf
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-28 14:02:42 +0000

    added more variables and tergeted them individually for gsap animations.

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger,TextPlugin)
     // gsap code here!
-    const page_title = document.querySelector('.title')
-    const hero_info = document.querySelector('.hero_info')
+    const header_text = document.querySelector('.header_text')
+
+    const hero_top_border = document.querySelector('.hero_top_border')
+    const hero_title = document.querySelector('.hero_title')
+    const hero_subtitle = document.querySelector('.hero_subtitle')
+    const hero_bottom_border = document.querySelector('.hero_bottom_border')
+
     const hero_question = document.querySelector('.hero_question')
     const option_one = document.querySelector('.option_one')
     const option_two = document.querySelector('.option_two')
@@ -12,29 +17,49 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     const timeline = gsap.timeline()
 
-    timeline.to(page_title, {
+    timeline.to(header_text, {
         text: `<i class="fa-solid fa-handshake"></i> Welcome!`,
+        duration: 1
+    })
+    .fromTo(hero_top_border, {
+        x: 500, 
+        opacity: 0
+    }, {
+        x: 0,
+        opacity: 1, 
+        duration: 0.4
+    })
+    .to(hero_title, {
+        text: 'My Name Is David.',
         duration: 1.2
     })
-    .to(hero_info, {
+    .to(hero_subtitle, {
+        text: `I am a software Developer <i class="fa-regular fa-floppy-disk"></i>`,
+        duration: 1.2
+    })
+    .fromTo(hero_bottom_border, {
+        x: -500,
+        opacity: 0
+    }, {
+        x: 0,
         opacity: 1,
-        duration: 1.2,
+        duration: 0.4
     })
     .to(hero_question, {
         text: "Please select an option...",
-        duration: 1.2
+        duration: 1
     })
     .to(option_one, {
         opacity: 1,
-        duration: 0.7
+        duration: 0.6
     })
     .to(option_two, {
         opacity: 1,
-        duration: 0.7
+        duration: 0.6
     })
     .to(option_three, {
         opacity: 1,
-        duration: 0.7
+        duration: 0.6
     })
 
 });
