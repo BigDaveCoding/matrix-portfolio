4d02a6f01df58bb7b7025cebca082c25fb243397
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-28 13:06:24 +0000

    added animations to a timeline so they happen one after the other.

--- script.js
+++ script.js
@@ -6,20 +6,35 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const page_title = document.querySelector('.title')
     const hero_info = document.querySelector('.hero_info')
     const hero_question = document.querySelector('.hero_question')
+    const option_one = document.querySelector('.option_one')
+    const option_two = document.querySelector('.option_two')
+    const option_three = document.querySelector('.option_three')
 
-    gsap.to(page_title, {
-        text: 'Welcome...',
-        duration: 2
-    });
+    const timeline = gsap.timeline()
 
-    gsap.to(hero_info, {
+    timeline.to(page_title, {
+        text: `<i class="fa-solid fa-handshake"></i> Welcome!`,
+        duration: 1.2
+    })
+    .to(hero_info, {
         opacity: 1,
-        duration: 1,
+        duration: 1.2,
+    })
+    .to(hero_question, {
+        text: "Please select an option...",
+        duration: 1.2
+    })
+    .to(option_one, {
+        opacity: 1,
+        duration: 0.7
+    })
+    .to(option_two, {
+        opacity: 1,
+        duration: 0.7
+    })
+    .to(option_three, {
+        opacity: 1,
+        duration: 0.7
     })
 
-    gsap.to(hero_question, {
-        text: "Please select an option",
-        duration: 2
-    })
-
-   });
+});
