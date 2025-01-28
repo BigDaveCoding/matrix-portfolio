e8ce1292fc09a768d2a7bf03488f27ecac65cbb7
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-28 12:50:56 +0000

    testing gsap animations

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger,TextPlugin)
     // gsap code here!
     const page_title = document.querySelector('.title')
+    const hero_info = document.querySelector('.hero_info')
+    const hero_question = document.querySelector('.hero_question')
 
     gsap.to(page_title, {
         text: 'Welcome...',
         duration: 2
     });
+
+    gsap.to(hero_info, {
+        opacity: 1,
+        duration: 1,
+    })
+
+    gsap.to(hero_question, {
+        text: "Please select an option",
+        duration: 2
+    })
 
    });
