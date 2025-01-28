10619171ada7d912fe59ae681b5ce06cc8e99c9e
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-28 12:24:15 +0000

    installed gsap and font awesome + testing them

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger,TextPlugin)
+    // gsap code here!
+    const page_title = document.querySelector('.title')
+
+    gsap.to(page_title, {
+        text: 'Welcome...',
+        duration: 2
+    });
+
+   });
